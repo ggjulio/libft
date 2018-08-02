a9733f5264efbddbee6a8f647a44a8a20cfdb97d
juliogg <gonzalez.julio89@hotmail.fr>
2018-08-02 17:51:00 +0200

    libft.h addede

---
+++ libft.h
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jugonzal <gonzalez.julio89@hotmail.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/01 01:58:46 by jugonzal          #+#    #+#             */
+/*   Updated: 2018/08/01 01:58:46 by jugonzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char const *s);
+void	ft_putnbr(int n);
+size_t	ft_strlen(const char *s);
+
+
+/*
+
+memset
+bzero
+memcpy
+memccpy
+memmove
+memchr
+memcmp
+strdup
+strcpy
+strncpy
+strcat
+strncat
+strlcat
+strchr
+strrchr
+strstr
+strnstr
+strcmp
+strncmp
+atoi
+isalpha
+isdigit
+isalnum
+isascii
+isprint
+toupper
+tolower
+
+*/
+
+void *ft_memalloc(size_t size);
+void ft_memdel(void **ap);
+char *ft_strnew(size_t size);
+void ft_strdel(char **as);
+void ft_strclr(char *s);
+void ft_striter(char *s, void (*f)(char *));
+void ft_striteri(char *s, void (*f)(unsigned int, char *));
+char *ft_strmap(char const *s, char (*f)(char));
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int ft_strequ(char const *s1, char const *s2);
+int ft_strnequ(char const *s1, char const *s2, size_t n);
+char *ft_strsub(char const *s, unsigned int start, size_t len);
+char *ft_strjoin(char const *s1, char const *s2);
+char *ft_strtrim(char const *s);
+char **ft_strsplit(char const *s, char c);
+char *ft_itoa(int n);
+
+void ft_putchar_fd(char c, int fd);
+
+void ft_putstr_fd(char const *s, int fd);
+
+void ft_putendl(char const *s);
+void ft_putendl_fd(char const *s, int fd);
+
+void ft_putnbr_fd(int n, int fd);
+
+/*
+
+//Bonus
+
+typedef struct      s_list
+{
+	void            *content;
+	size_t          content_size;
+	struct s_list   *next;
+}                   t_list;
+
+ft_lstnew
+ft_lstdelone
+ft_lstdel
+ft_lstadd
+ft_lstiter
+ft_lstmap
+
+Si vous réussissez parfaitement la partie obligatoire et la partie bonus, vous êtes
+encouragés à ajouter d’autres fonctions qui vous paraissent utiles pour agrandir votre
+bibliothèque. Exemples : une version de ft_strsplit qui renvoie une liste de chaines au
+lieu d’un tableau de chaines, la fonction ft_lstfold similaire à la fonction reduce de
+Python et à la fonction List.fold_left d’OCaml (attention aux fuites mémoires !), des
+fonctions de manipulation de tableaux, de piles, de files, de maps, de tables de hash, etc.
+La limite est votre imagination.
+
+
+*/
+
+#endif
