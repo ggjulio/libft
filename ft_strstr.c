efad25d3b12f41b24c8711279fec34eba555e979
juliogg <gonzalez.julio89@hotmail.fr>
2018-08-03 02:49:38 +0200

    done : memalloc, memdel, strstr

---
+++ ft_strstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jugonzal <gonzalez.julio89@hotmail.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/03 01:57:20 by jugonzal          #+#    #+#             */
+/*   Updated: 2018/08/03 01:57:20 by jugonzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	int i;
+	int j;
+
+	i = 0;
+	if (!ft_strlen(needle))
+		return ((char *)haystack);
+	while (haystack[i])
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j])
+		{
+			if (!needle[j + 1])
+				return ((char *)&haystack[i]);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
