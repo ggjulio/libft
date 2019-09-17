8f25577b4530e2ab288f10a853a553eb9ca3230e
Julio Gonzalez <gonzalez.julio8959@gmail.com>
2019-09-17 09:29:10 -0700

    Error fixed in memchr and memmove. memchr : comparing char by casting them in unsigned char. memmove : removing malloc

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/13 20:29:50 by juligonz          #+#    #+#             */
-/*   Updated: 2019/08/13 20:29:51 by juligonz         ###   ########.fr       */
+/*   Updated: 2019/09/17 09:20:15 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char *t;
+	size_t i;
 
-	if (!(t = malloc(sizeof(char) * n)))
-		return (NULL);
-	ft_memcpy(t, src, n);
-	ft_memcpy(dest, t, n);
-	free(t);
-	return (dest);
+	i = 0;
+	if ((unsigned char *)dest > (unsigned char *)src)
+		while (n-- > 0)
+			((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
+	else if ((unsigned char *)dest < (unsigned char *)src)
+		while (i < n)
+		{
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+			i++;
+		}
+	return ((unsigned char *)dest);
 }
