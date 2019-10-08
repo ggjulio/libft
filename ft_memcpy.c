32a94cae0e885ec7cf87a2ec69009d86eabf9345
Julio GONZALEZ GOMEZ <juligonz@e2r2p19.42.fr>
2019-10-08 15:13:31 +0200

    All Libft Done And fixed (memcpy). Ready to the new subject

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/13 20:25:39 by juligonz          #+#    #+#             */
-/*   Updated: 2019/08/13 20:25:39 by juligonz         ###   ########.fr       */
+/*   Updated: 2019/10/08 15:05:46 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	char *d;
 	char *s;
 
+	if (!dest && !src)
+		return (NULL);
 	d = (char *)dest;
 	s = (char *)src;
 	while (n-- > 0)
