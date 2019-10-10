6a2024531b459cb0d346da49c7a19a28a698429c
Julio GONZALEZ GOMEZ <juligonz@e1r9p17.42.fr>
2019-10-10 15:54:21 +0200

    add back fixed

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: juligonz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 19:17:49 by juligonz          #+#    #+#             */
-/*   Updated: 2019/10/10 14:58:54 by juligonz         ###   ########.fr       */
+/*   Updated: 2019/10/10 15:44:14 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ void	ft_lstadd_back(t_list **alst, t_list *new)
 {
 	while ((*alst)->next)
 		alst = &(*alst)->next;
-	(*alst)->content = new;
+	(*alst)->next = new;
 }
