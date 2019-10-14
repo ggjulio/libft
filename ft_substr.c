9c914e77f38a4a4a3c30d22d73630b493c1233ff
Julio Gonzalez <gonzalez.julio8959@gmail.com>
2019-10-14 17:05:59 +0200

    substr refactorized. NEED NORMINETTE

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: juligonz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 18:56:38 by juligonz          #+#    #+#             */
-/*   Updated: 2019/10/11 19:36:32 by juligonz         ###   ########.fr       */
+/*   Updated: 2019/10/14 17:04:25 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,12 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
-	size_t	s_len;
 	char	*res;
 
-	s_len = ft_strlen(s);
-	if (s_len < start)
+	if (ft_strlen(s) < start)
 	{
-		if (!(res = malloc(1)))
+		if (!(res = ft_strnew(0)))
 			return (NULL);
-		res[0] = '\0';
 	}
 	else
 	{
