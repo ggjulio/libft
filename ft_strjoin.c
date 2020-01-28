0f04331b5c272c3f6b74f41a4985c0f8c554868e
ggjulio <juligonz@student.42.fr>
2020-01-28 20:23:46 +0100

    ft_strjoin.c : malloc allocation size fixed

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/24 08:54:45 by juligonz          #+#    #+#             */
-/*   Updated: 2019/10/09 22:27:35 by juligonz         ###   ########.fr       */
+/*   Updated: 2020/01/28 19:40:07 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char *res;
 
-	if (!(res = (char *)malloc(ft_strlen(s1) + ft_strlen(s1) + 1)))
+	if (!(res = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
 		return (NULL);
 	ft_strcpy(res, s1);
 	ft_strcat(res, s2);
