cd93c655f1332022fdbb674151f4c7f750c31170
ggjulio <juligonz@student.42.fr>
2020-02-01 21:41:08 +0100

    ft_valid_extention segfault fixed when no dot in str

--- ft_valid_extention.c
+++ ft_valid_extention.c
@@ -6,7 +6,7 @@
 /*   By: juligonz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/27 17:47:23 by juligonz          #+#    #+#             */
-/*   Updated: 2020/01/27 17:53:50 by juligonz         ###   ########.fr       */
+/*   Updated: 2020/02/01 20:07:00 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		ft_valid_extention(char *filename, char *extention)
 	char	*file_ext;
 
 	file_ext = ft_strrchr(filename, '.');
-	if (ft_strcmp(file_ext, extention) == 0)
+	if (file_ext != NULL && ft_strcmp(file_ext, extention) == 0)
 		return (1);
 	return (0);
 }
