ccad2b272006a00a712eef4078e3d20a9b19142a
ggjulio <juligonz@student.42.fr>
2020-02-15 09:14:35 +0100

    ft_lstpop_front fixed

--- ft_lstpop_front.c
+++ ft_lstpop_front.c
@@ -6,17 +6,18 @@
 /*   By: juligonz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/14 10:57:06 by juligonz          #+#    #+#             */
-/*   Updated: 2020/02/14 12:52:24 by juligonz         ###   ########.fr       */
+/*   Updated: 2020/02/14 16:14:16 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstpop_front(t_list **lst)
+t_list	*ft_lstpop_front(t_list **alst)
 {
 	t_list *result;
 
-	result = *lst;
-	*lst = (*lst)->next;
+	result = *alst;
+	if (*alst != NULL)
+		*alst = (*alst)->next;
 	return (result);
 }
