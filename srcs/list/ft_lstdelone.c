2d542417111529ae1c4577f048bcb13f2e377ba2
ggjulio <juligonz@student.42.fr>
2020-08-20 16:07:16 +0200

    ft_lstdelone :  Now not deleting content if del param is NULL

--- srcs/list/ft_lstdelone.c
+++ srcs/list/ft_lstdelone.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juligonz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 19:47:02 by juligonz          #+#    #+#             */
-/*   Updated: 2019/10/10 13:42:22 by juligonz         ###   ########.fr       */
+/*   Updated: 2020/08/20 16:06:26 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	del(lst->content);
+	if (del != NULL)
+		del(lst->content);
 	free(lst);
-	lst = NULL;
 }
